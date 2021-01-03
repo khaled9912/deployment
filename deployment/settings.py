e8c3e4a628db6f71a69320555db84010c33d5bff
khaled9912 <71382421+khaled9912@users.noreply.github.com>
2021-01-03 20:28:17 +0200

    Update settings.py

--- deployment/settings.py
+++ deployment/settings.py
@@ -139,22 +139,22 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 # DATABASES #
 #############
 
-DATABASES = {
-    "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "alexgreen",
-        # Not used with sqlite3.
-        "USER": "khaledmourad",
-        # Not used with sqlite3.
-        "PASSWORD": "passwordpostgres",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "database-1.cqutv8fmtcxv.us-east-2.rds.amazonaws.com",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         # DB name or path to database file if using sqlite3.
+#         "NAME": "alexgreen",
+#         # Not used with sqlite3.
+#         "USER": "khaledmourad",
+#         # Not used with sqlite3.
+#         "PASSWORD": "passwordpostgres",
+#         # Set to empty string for localhost. Not used with sqlite3.
+#         "HOST": "database-1.cqutv8fmtcxv.us-east-2.rds.amazonaws.com",
+#         # Set to empty string for default. Not used with sqlite3.
+#         "PORT": "5432",
+#     }
+# }
 
 
 
