2cd62606304ddb776b1eae60d70f657b8944c77f
khaled9912 <71382421+khaled9912@users.noreply.github.com>
2021-01-06 19:37:38 +0200

    Update local_settings.py

--- deployment/local_settings.py
+++ deployment/local_settings.py
@@ -7,18 +7,34 @@ DEBUG = False
 SECRET_KEY = "9i-0s1rr8#sp&(j$0n3t8s*-t23g0!i9&rs)1=!h(cirx3b$x3"
 NEVERCACHE_KEY = "#_j9)qpfw_ems2(qt^8pjviw*cdg5@fr)^r1of!6zg=-(7bg*9"
 
+# DATABASES = {
+#     "default": {
+#         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         # DB name or path to database file if using sqlite3.
+#         "NAME": "d37hr7jdbn5pfp",
+#         # Not used with sqlite3.
+#         "USER": "vibcvgmliwxvmm",
+#         # Not used with sqlite3.
+#         "PASSWORD": "daacbab5eb7c252d0d8cf199e4eb0d51dcbd81e4ef51daa41ad80ee7e20f91b1",
+#         # Set to empty string for localhost. Not used with sqlite3.
+#         "HOST": "ec2-52-6-75-198.compute-1.amazonaws.com",
+#         # Set to empty string for default. Not used with sqlite3.
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
     "default": {
         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         # DB name or path to database file if using sqlite3.
-        "NAME": "d37hr7jdbn5pfp",
+        "NAME": "alexgreen",
         # Not used with sqlite3.
-        "USER": "vibcvgmliwxvmm",
+        "USER": "khaled",
         # Not used with sqlite3.
-        "PASSWORD": "daacbab5eb7c252d0d8cf199e4eb0d51dcbd81e4ef51daa41ad80ee7e20f91b1",
+        "PASSWORD": "123698745KH",
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "ec2-52-6-75-198.compute-1.amazonaws.com",
+        "HOST": "database-2.cqutv8fmtcxv.us-east-2.rds.amazonaws.com",
         # Set to empty string for default. Not used with sqlite3.
         "PORT": "5432",
     }
